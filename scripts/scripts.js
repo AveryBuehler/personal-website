8696227063c9c5abe37d9724f1440502df8e614a
averybuehler <averybuehler@comcast.net>
2018-12-02 23:00:35 -0500

    Updating for 200% zoom on 4k monitors

---
+++ scripts/scripts.js
@@ -0,0 +1,5 @@
+if(screen.availWidth >= 1100 && screen.availWidth <= 2000) {
+  console.log("1100-2000");
+  document.getElementById('displayZoomLG').style.paddingRight = "0";
+  document.getElementById('slice').style.background = "linear-gradient(63deg, rgb(241, 57, 109) 31%, rgba(255, 0, 0, 0) 0)";
+}
